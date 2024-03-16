d894be9f53bf2d29e059e7a6b24345babe36afe4
hak2711 <hak271111@gmail.com>
2024-03-16 10:26:11 +0900

    initial commit

---
+++ src/main/java/com/ewhathon/notegather/NotegatherApplication.java
@@ -0,0 +1,13 @@
+package com.ewhathon.notegather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotegatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotegatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewhathon/notegather/NotegatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewhathon.notegather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotegatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
