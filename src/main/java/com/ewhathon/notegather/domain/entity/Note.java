3a1d91505976e399d9159036d3e1f53dcc546c23
uommou <ffffff.990917@gmail.com>
2024-03-16 20:06:50 +0900

    test

--- src/main/java/com/ewhathon/notegather/domain/entity/Note.java
+++ src/main/java/com/ewhathon/notegather/domain/entity/Note.java
@@ -1,10 +1,7 @@
 package com.ewhathon.notegather.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +9,7 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 @Table(name = "NOTE_TB")
 public class Note {
     @Id
